da18a3a4c2c67795d29a643b4b12b2b16d69d32a
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-01-23 14:38:03 +0700

    fix: home page

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -2,21 +2,21 @@
 import Layout from '../layout';
 
 const Home = () => {
-  return (
-    <Layout>
-      {/* Dashboard */}
-      <div className="space-y-4">
-        {/* Top Boxes */}
-        <div className="grid grid-cols-3 gap-4">
-          <div className="bg-blue-900 h-24 rounded-md"></div>
-          <div className="bg-blue-900 h-24 rounded-md"></div>
-          <div className="bg-blue-900 h-24 rounded-md"></div>
-        </div>
+  return ( <div></div>
+    // <Layout>
+    //   {/* Home */}
+    //   <div className="space-y-4">
+    //     {/* Top Boxes */}
+    //     <div className="grid grid-cols-3 gap-4">
+    //       {/* <div className="bg-blue-900 h-24 rounded-md"></div>
+    //       <div className="bg-blue-900 h-24 rounded-md"></div>
+    //       <div className="bg-blue-900 h-24 rounded-md"></div> */}
+    //     </div>
 
-        {/* Main Panel */}
-        <div className="bg-gray-300 h-96 rounded-md"></div>
-      </div>
-    </Layout>
+    //     {/* Main Panel */}
+    //     <div className="bg-gray-300 h-96 rounded-md"></div>
+    //   </div>
+    // </Layout>
   );
 };
 
