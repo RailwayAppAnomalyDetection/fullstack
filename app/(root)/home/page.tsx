f4f1245686f09ce4d7ab113dd2435be8741761c7
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-01-23 16:58:26 +0700

    feat: openstreetmap initial setup

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,7 +1,16 @@
+"use client";
 // pages/index.tsx
 //import Layout from "../layout";
+import dynamic from "next/dynamic";
+
+// Dynamically import Leaflet components
+const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import('react-leaflet').then((mod) => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import('react-leaflet').then((mod) => mod.Popup), { ssr: false });
 
 const Home = () => {
+  const position:[number,number] = [51.505, -0.09];
   return (
     
     <div className="space-y-4">
@@ -13,7 +22,16 @@ const Home = () => {
       </div>
 
       {/* Main Panel */}
-      <div className="bg-gray-300 h-96 rounded-md"></div>
+      <div className="bg-gray-300 h-96 rounded-md overflow-hidden">
+        <MapContainer center={position} zoom={13} className="h-full w-full">
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/>
+          <Marker position={position}>
+            <Popup>
+              A sample popup for this location
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
 
   );
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const layout = ({children}:{children: React.ReactNode}) => {
   return (
     <div className="flex h-screen overflow-hidden">
