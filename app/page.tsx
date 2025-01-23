580910c596ecb018e2209c8ab91cc0d84ed11d63
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-01-23 15:44:36 +0700

    fix: poppins

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
     <>
       <div className="flex h-screen items-center justify-center bg-array-100">
         <div className="bg-blue-900 text-white p-8 rounded-lg shadow-lg text-center">
-          <h1 className="text-2xl font-bold mb-6">Railway Comfort Indicator</h1>
-          <button onClick={handleStart} className="bg-gray-300 text-blue-900 font-semibold px-6 py-2 rounded-md hover:bg-gray-400">
+          <h1 className="text-2xl font-bold mb-6 font-poppins">Railway Comfort Indicator</h1>
+          <button onClick={handleStart} className="font-poppins bg-gray-300 text-blue-900 font-semibold px-6 py-2 rounded-md hover:bg-gray-400">
             START
           </button>
         </div>
