e6292f0fe6a5d60c534ef302cc9774ed63a6e6f5
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-01-23 15:04:49 +0700

    feat: start page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+"use client"
 // import Hello from "./components/hello";
+import {useRouter} from "next/navigation"
 
 export default function Home() {
+  const router = useRouter();
+  const handleStart = () => {
+    router.push("/home");
+  }
   return (
     <>
       <div className="flex h-screen items-center justify-center bg-array-100">
         <div className="bg-blue-900 text-white p-8 rounded-lg shadow-lg text-center">
           <h1 className="text-2xl font-bold mb-6">Railway Comfort Indicator</h1>
-          <button className="bg-gray-300 text-blue-900 font-semibold px-6 py-2 rounded-md hover:bg-gray-400">
+          <button onClick={handleStart} className="bg-gray-300 text-blue-900 font-semibold px-6 py-2 rounded-md hover:bg-gray-400">
             START
           </button>
         </div>
