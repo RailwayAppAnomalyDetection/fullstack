79448d5a6ce7917fa6c4900ee46a9e55a2506049
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-01-23 14:38:24 +0700

    feat: add start page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Hello from "./components/hello";
+// import Hello from "./components/hello";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl font-bold">Hello World</h1>
-      <Hello />
+      <div className="flex h-screen items-center justify-center bg-array-100">
+        <div className="bg-blue-900 text-white p-8 rounded-lg shadow-lg text-center">
+          <h1 className="text-2xl font-bold mb-6">Railway Comfort Indicator</h1>
+          <button className="bg-gray-300 text-blue-900 font-semibold px-6 py-2 rounded-md hover:bg-gray-400">
+            START
+          </button>
+        </div>
+      </div>
+      {/* <Hello /> */}
     </>
   );
 }
