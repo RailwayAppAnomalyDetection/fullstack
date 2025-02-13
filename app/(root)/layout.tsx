22f30880b07b656f118c1ac6b208f91130cad7ea
krystoliz <109867611+krystoliz@users.noreply.github.com>
2025-02-13 10:23:10 +0700

    feat: button for view100mdata

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -25,7 +25,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               <Link href='/viewdata'>
               <button className={`w-full text-left hover:bg-blue-700 p-2 rounded ${isActive("/viewdata") ? "bg-blue-600" : "hover:bg-blue-700"}`}>View Data</button>
               </Link>
-              <button className="text-left hover:bg-blue-700 p-2 rounded">Regular Train</button>
+              <Link href='/view100mdata'>
+              <button className={`w-full text-left hover:bg-blue-700 p-2 rounded ${isActive("/view100mdata") ? "bg-blue-600" : "hover:bg-blue-700"}`}>View Data for Each 100m</button>
+              </Link>
               <Link href='/training'>
               <button className="text-left hover:bg-blue-700 p-2 rounded">Training</button>
               </Link>
